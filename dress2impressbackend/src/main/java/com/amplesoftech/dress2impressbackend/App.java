46f289f60e703e55539e028f978f06faa859afec
supriya <supriyasable99@gmail.com>
2018-11-28 11:40:13 +0530

    Project setup completed successfully

---
+++ dress2impressbackend/src/main/java/com/amplesoftech/dress2impressbackend/App.java
@@ -0,0 +1,13 @@
+package com.amplesoftech.dress2impressbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
